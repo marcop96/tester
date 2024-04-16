e501e3e904a50d350fa9857bfb83007e22646463
“Marco <marco.pianaroli96@gmail.com>
2024-04-16 10:44:15 +0200

    feat: adds types

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+export interface Inventory {
+  items: object[];
+  gold: number;
+}
+
+export interface Item {
+  id: number;
+  name: string;
+  type: string;
+  price: number;
+  quantity?: number;
+  exp?: number;
+}
