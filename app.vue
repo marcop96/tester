e957b89565088a4cc11a0f4bde5e946a98ec022a
“Marco <marco.pianaroli96@gmail.com>
2024-04-16 10:40:34 +0200

    feat: progress bars

--- app.vue
+++ app.vue
@@ -1,24 +1,53 @@
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { reactive } from "vue";
 import { Progress } from "@/components/ui/progress";
 
-const progress = ref(0);
-onMounted(() => {
+const progress = reactive({
+  one: 0,
+  two: 0,
+});
+
+let isProgressRunning = false;
+
+function runProgress(progressKey: keyof typeof progress, duration = 1000) {
+  if (isProgressRunning) return;
+  isProgressRunning = true;
+
   let currentProgress = 0;
   const interval = setInterval(() => {
-    currentProgress += 1; // Increase by 25 every second
+    currentProgress += 1;
     if (currentProgress <= 100) {
-      progress.value = currentProgress;
+      progress[progressKey] = currentProgress;
     } else {
       clearInterval(interval);
+      progress[progressKey] = 0;
+      isProgressRunning = false;
     }
-  }, 10); // Run every 1 second
-  currentProgress = 0;
-});
+  }, duration / 100);
+}
 </script>
 
 <template>
   <div class="p-4 mx-auto">
-    <Progress v-model="progress" class="w-3/5" />
+    <div class="p-4">
+      <Progress v-model="progress.one" class="w-3/5 h-10" />
+    </div>
+    <div class="p-4">
+      <Progress v-model="progress.two" class="w-3/5 h-10" />
+    </div>
+    <button
+      @click="runProgress('one', 50)"
+      class="mt-4 p-4"
+      :disabled="isProgressRunning"
+    >
+      Run Progress 1
+    </button>
+    <button
+      @click="runProgress('two', 2000)"
+      class="mt-4 p-4"
+      :disabled="isProgressRunning"
+    >
+      Run Progress 2
+    </button>
   </div>
 </template>
