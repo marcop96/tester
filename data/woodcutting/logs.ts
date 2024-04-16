a190dbbcfcf6d75eb4fbf8fd559edc681cf83768
“Marco <marco.pianaroli96@gmail.com>
2024-04-16 10:45:09 +0200

    feat:adds logs info for woodcutting

---
+++ data/woodcutting/logs.ts
@@ -0,0 +1,11 @@
+import type { Item } from "../../types";
+const logs: Item[] = [
+  { id: 100, price: 4, type: "log", name: "Normal Log", exp: 25 },
+  { id: 101, price: 4, type: "log", name: "Oak Log", exp: 25 },
+  { id: 102, price: 4, type: "log", name: "Willow Log", exp: 45 },
+  { id: 103, price: 4, type: "log", name: "Maple Log", exp: 65 },
+  { id: 104, price: 4, type: "log", name: "Yew Log", exp: 85 },
+  { id: 105, price: 4, type: "log", name: "Magic Log", exp: 105 },
+];
+
+export default logs;
