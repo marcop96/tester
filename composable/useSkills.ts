d53424c9aaf9ca5db02c3ef028b32c255dcddad7
“Marco <marco.pianaroli96@gmail.com>
2024-04-20 18:01:05 +0200

    feat:creates skills composable

---
+++ composable/useSkills.ts
@@ -0,0 +1,24 @@
+import { ref } from "vue";
+import type { Skill } from "../types.d.ts";
+
+export default function useSkills() {
+  const skills = ref<Record<string, Skill>>({
+    woodcutting: {
+      id: 1,
+      level: 1,
+      exp: 0,
+    },
+    mining: {
+      id: 2,
+      level: 1,
+      exp: 0,
+    },
+    smithing: {
+      id: 3,
+      level: 1,
+      exp: 0,
+    },
+  });
+
+  return { skills };
+}
