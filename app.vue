5b326b200276d224cd8cee97c3607c95342c5d10
“Marco <marco.pianaroli96@gmail.com>
2024-04-21 12:33:44 +0200

    chore: makes visually easier to test

--- app.vue
+++ app.vue
@@ -3,70 +3,19 @@ import { Progress } from "@/components/ui/progress";
 import { useInventory } from "./composable/useInventory";
 import type { Item } from "./types";
 import logs from "./data/woodcutting/logs";
-
+import useAction from "./composable/useAction";
 // Get the gold value from the inventory
-const gold = useInventory().gold;
-
 // Get the inventory items from the inventory
-const inventory = useInventory().inventory;
 
-// Define the selected wood item
-// const selectedWood = ref<Item>({
-//   id: 100,
-//   price: 4,
-//   type: "log",
-//   name: "Normal Log",
-//   exp: 25,
-// });
-
+const { addItemToInventory, removeItemFromInventory, itemCount } =
+  useInventory();
+const { inventory } = useAction();
 // Define the progress object
 const progress = reactive({
   one: 0,
 });
 
 // Define the chopping state
-let isChopping = false;
-
-// Function to run the progress
-async function runProgress(log: Item, duration = 1000) {
-  console.log(log.name);
-  // If already chopping, return
-  if (isChopping) return;
-  isChopping = true;
-
-  let currentProgress = 0;
-  // Check if the player has an axe in the inventory
-  if (inventory.value.some((item: Item) => item.type === "axe")) {
-    const interval = setInterval(() => {
-      currentProgress += 1;
-      if (currentProgress <= 100) {
-        // Update the progress value
-        progress.one = currentProgress;
-      } else {
-        clearInterval(interval);
-        // Reset the progress value
-        progress.one = 0;
-        isChopping = false;
-        // Find the selected wood item in the inventory
-        const logItem = inventory.value.find(
-          (item: Item) => item.name === `${log.name}`
-        );
-        if (logItem && logItem.quantity) {
-          // Increase the quantity of the selected wood item
-          logItem.quantity += 1;
-        } else {
-          // Add the selected wood item to the inventory
-          inventory.value.push({
-            ...log,
-            quantity: 1,
-          });
-        }
-      }
-    }, duration / 100);
-  } else {
-    console.error("You need a bronze axe or better to cut down a log.");
-  }
-}
 </script>
 
 <template>
@@ -81,14 +30,13 @@ async function runProgress(log: Item, duration = 1000) {
         v-for="log in logs"
         :key="log.id"
         class="bg-gray-400 rounded-sm gap-4 m-2 w-32 h-32"
-        @click="runProgress(log, log.difficulty)"
-        :disabled="isChopping"
+        @click="useAction().runAction(log, log.difficulty)"
       >
         {{ log.name }}
       </button>
     </div>
   </div>
-  <div>Gold: {{ gold }}</div>
+  <div>itemcount: {{ itemCount }}</div>
   <div>
     Inventory:
     <ul>
