f622cc5c1d528a5c5bb3323317d4f28eb364f23a
Marco <me@marcopianaroli.com>
2024-04-28 20:18:05 +0200

    feat:creates default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,30 @@
+<script setup lang="ts">
+import useSkills from "../composable/useSkills";
+const { skills, skillNames, activeSkill } = useSkills();
+
+function changeSkill(skill: string) {
+  activeSkill.value = skill;
+
+  console.log(activeSkill.value);
+}
+</script>
 <template>
-  <div class="h-screen fixed w-48 left-0 top-0 bg-green-400 bg-opacity-75 p-4">
-    <transition name="fade">
-      <div
-        v-if="open"
-        class="flex flex-row max-w-lg transition-color duration-1000"
-      >
-        <div class="flex-1">
-          <ul>
-            <li class="block transform origin-center font-bold">Cooking</li>
-            <li class="block transform origin-center font-bold">Fishing</li>
-            <li class="block transform origin-center font-bold">Hiking</li>
-          </ul>
-        </div>
-      </div>
-    </transition>
+  <div class="flex">
+    <div class="h-screen w-1/5 bg-green-400 bg-opacity-75 p-4">
+      <ul>
+        <li
+          v-for="name in skillNames"
+          @click="changeSkill(name)"
+          class="block transform origin-center font-bold"
+        >
+          {{ name }}
+        </li>
+      </ul>
+    </div>
+    <div class="flex-1">
+      <NuxtPage />
+    </div>
   </div>
-  <button class="absolute" @click="toggle">toggle</button>
-  <!-- <div>
-    <NuxtPage />
-  </div> -->
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-const open = ref(true);
-
-const toggle = () => {
-  open.value = !open.value;
-};
-</script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
+<style></style>
