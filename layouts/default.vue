6241d49c1aaef1ae5a3c6ea392441bb2ff74fc07
Marco <me@marcopianaroli.com>
2024-04-29 19:07:17 +0200

    feat: uses global store to render proper resources

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import useSkills from "../composable/useSkills";
-const { skills, skillNames, activeSkill } = useSkills();
-
-function changeSkill(skill: string) {
-  activeSkill.value = skill;
-
-  console.log(activeSkill.value);
-}
+import { useSkillsStore } from "../composable/useSkills";
+const store = useSkillsStore();
 </script>
 <template>
   <div class="flex">
     <div class="h-screen w-1/5 bg-green-400 bg-opacity-75 p-4">
       <ul>
         <li
-          v-for="name in skillNames"
-          @click="changeSkill(name)"
-          class="block transform origin-center font-bold"
+          v-for="name in store.skillNames"
+          @click="store.changeActiveSkill(name)"
+          class="block transform origin-center font-bold hover:pointer-select"
         >
           {{ name }}
         </li>
       </ul>
     </div>
-    <div class="flex-1">
-      <NuxtPage />
-    </div>
+    <div class="flex-1"><NuxtPage /></div>
   </div>
 </template>
-
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,16 @@ import { useInventory } from "../composable/useInventory";
 import useAction from "../composable/useAction";
 import InfoActionButton from "../components/ui/InfoActionButton.vue";
 import resources from "../data/resources.json";
-import useSkills from "../composable/useSkills";
+import { useSkillsStore } from "../composable/useSkills";
 const { addItemToInventory, removeItemFromInventory, itemCount } =
   useInventory();
 const { runAction, inventory, progress, activeItem } = useAction();
 
-const { skills, skillNames, activeSkill } = useSkills();
+const store = useSkillsStore();
+
+setTimeout(() => {
+  console.log(store);
+}, 1000);
 </script>
 
 <template>
@@ -21,7 +25,7 @@ const { skills, skillNames, activeSkill } = useSkills();
 
     <div class="grid grid-cols-3 p-4">
       <InfoActionButton
-        v-for="res in resources.filter((r) => r.skill === activeSkill)"
+        v-for="res in resources.filter((r) => r.skill === store.activeSkill)"
         :key="res.id"
         action="chop"
         :item="res"
