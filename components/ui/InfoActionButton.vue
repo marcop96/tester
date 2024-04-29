81415c9e3e06abe9e947e3fe3526db2f5ca4e64c
Marco <me@marcopianaroli.com>
2024-04-29 20:35:57 +0200

    feat: uses composable to render action name

--- components/ui/InfoActionButton.vue
+++ components/ui/InfoActionButton.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import type { Item } from "@/types";
-defineProps<{
+import type { Item, SkillName } from "@/types";
+const props = defineProps<{
   item: Item;
+  activeSkill: string | null;
   activeItem: Item | null;
 }>();
+
+const itemStatus = computed(() => {
+  if (props.activeItem) {
+    if (props.activeItem && props.activeItem.name === props.item.name) {
+      if (props.activeSkill === "woodcutting") {
+        return "Chopping";
+      } else if (props.activeSkill === "mining") {
+        return "Mining";
+      }
+    }
+  }
+
+  return "Waiting";
+});
 </script>
 
 <template>
@@ -25,9 +40,7 @@ defineProps<{
         :class="{
           'text-green-500': activeItem && activeItem.name === item.name,
         }"
-        >{{
-          activeItem && activeItem.name === item.name ? "Chopping" : "Waiting"
-        }}</span
+        >{{ itemStatus }}</span
       >
     </div>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,15 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { Progress } from "../components/ui/progress";
 import { useInventory } from "../composable/useInventory";
 import useAction from "../composable/useAction";
 import InfoActionButton from "../components/ui/InfoActionButton.vue";
 import resources from "../data/resources.json";
 import { useSkillsStore } from "../composable/useSkills";
-const { addItemToInventory, removeItemFromInventory, itemCount } =
-  useInventory();
+// const { addItemToInventory, removeItemFromInventory, itemCount } =
+//   useInventory();
 const { runAction, inventory, progress, activeItem } = useAction();
 
 const store = useSkillsStore();
-
-setTimeout(() => {
-  console.log(store);
-}, 1000);
 </script>
 
 <template>
@@ -27,9 +22,9 @@ setTimeout(() => {
       <InfoActionButton
         v-for="res in resources.filter((r) => r.skill === store.activeSkill)"
         :key="res.id"
-        action="chop"
         :item="res"
         :activeItem="activeItem"
+        :activeSkill="store.activeSkill"
         @click="runAction(res)"
       ></InfoActionButton>
     </div>
